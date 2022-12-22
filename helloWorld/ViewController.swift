0d210045c57825aa17d00e0e4db1c0254f84209f
Stogov Anton <sloppybait@yandex.ru>
2022-12-22 22:44:44 +0300

    Refactor greetingButtonDidTaper -> greetingButtonDidTapper

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func greetingButtonDidTapped() {
+    }
+    
 }
 
