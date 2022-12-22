fe8f564f7e6061d9eca6f83d53e5ce2be9e4069d
Stogov Anton <sloppybait@yandex.ru>
2022-12-22 22:59:06 +0300

    Logic show and hide button

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -14,11 +14,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
     }
 
     @IBAction func greetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal)
+
     }
-    
 }
 
